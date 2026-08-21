99a1ba0d1555abcfc0252b4ff68e920377237a4b
Teyetdgd <blackstars345400@gmail.com>
2026-08-21 14:15:43 +0300

    Delete nexus-hub directory

--- nexus-hub/src/core/event_loop.h
+++
@@ -1,9 +0,0 @@
-#ifndef NEXUS_EVENT_LOOP_H
-#define NEXUS_EVENT_LOOP_H
-
-#include "types.h"
-
-void event_loop_run(NexusContext* ctx);
-
-#endif
-
--- nexus-hub/src/modules/network_diag.h
+++
@@ -1,7 +0,0 @@
-#ifndef NEXUS_NETWORK_DIAG_H
-#define NEXUS_NETWORK_DIAG_H
-
-int network_diag_run(void);
-
-#endif
-
